7d6461a4a308498216811ab5fcfd26bf2c5a6ec4
Sandipan <sandipanseth2004@gmail.com>
2025-05-30 20:39:19 +0530

    CORS

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -8,21 +8,13 @@ configDotenv();
 export const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: "https://code-reviewer-lac.vercel.app",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-  })
-);
-
+app.use(cors());
 
 app.get("/", async (req, res) => {
-    res.status(200).json({
-        success: "success",
-        message: "Welcome to the AI API"
-    });
+  res.status(200).json({
+    success: "success",
+    message: "Welcome to the AI API",
+  });
 });
 
 app.use("/api/ai", router);
